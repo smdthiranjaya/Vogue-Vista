1d33bca2fa6986d3211cf515fc5cd4e6040c1453
smdthiranjaya <s.m.d.thiranjaya@gmail.com>
2024-03-14 01:17:47 +0530

    Initial Commit

---
+++ Vogue Vista/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vogue Vista
+//
+//  Created by SMD Thiranjaya on BE 2567-03-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vogue Vista/Vogue_VistaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vogue_VistaApp.swift
+//  Vogue Vista
+//
+//  Created by SMD Thiranjaya on BE 2567-03-14.
+//
+
+import SwiftUI
+
+@main
+struct Vogue_VistaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

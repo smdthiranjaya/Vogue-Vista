a93ce075fcf2ed74a851af24bce62f80f0d41909
smdthiranjaya <s.m.d.thiranjaya@gmail.com>
2024-03-20 16:20:58 +0530

    Clean initial commit.

---
+++ Vogue Vista/StartupPageViewController.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+class StartupPageViewController: UIViewController {
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        return button
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        let stackView = UIStackView(arrangedSubviews: [signUpButton, loginButton])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc func handleSignUp() {
+        let signUpVC = SignUpViewController()
+        present(signUpVC, animated: true)
+    }
+    
+    @objc private func handleLogin() {
+        let loginVC = LoginViewController()
+        present(loginVC, animated: true)
+    }
+}
---
+++ Vogue Vista/ViewModels/CheckoutViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CheckoutViewModel.swift
+//  Vogue Vista
+//
+//  Created by SMD Thiranjaya on BE 2567-03-14.
+//
+
+import Foundation

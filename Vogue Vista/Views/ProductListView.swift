698879d1789c95866de19fcd0714138b69e1db1b
smdthiranjaya <s.m.d.thiranjaya@gmail.com>
2024-03-31 18:38:52 +0530

    Full clean main branch

--- Vogue Vista/Views/ProductListView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ProductListView.swift
-//  Vogue Vista
-//
-//  Created by SMD Thiranjaya on BE 2567-03-14.
-//
-
-import Foundation
--- Vogue Vista/Views/ShoppingCartView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ShoppingCartView.swift
-//  Vogue Vista
-//
-//  Created by SMD Thiranjaya on BE 2567-03-14.
-//
-
-import Foundation
